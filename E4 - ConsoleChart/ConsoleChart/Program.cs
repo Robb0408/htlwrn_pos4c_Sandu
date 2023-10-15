210788e582b926d6753e4d49f0c252990a406514
Robb0408 <emanuelrobertosandu@gmail.com>
2023-10-15 13:10:17 +0200

    finished E4, added comments

--- E4 - ConsoleChart/ConsoleChart/Program.cs
+++ E4 - ConsoleChart/ConsoleChart/Program.cs
@@ -1,20 +1,23 @@
-﻿string fileName;
-string? groupByCol, numericCol;
-List<string> lines = new();
+﻿using ConsoleChart;
 
-if (args.Length != 3)
+// Check argument count
+if (args.Length < 3 || args.Length > 4)
 {
     DisplayError("Not enough or too many arguments provided, expected 3.");
     Environment.Exit(1);
 }
-fileName = args[0];
+
+List<string> lines = new();
+string fileName = args[0];
+
+// Read the file and catch possible exceptions
 try
 {
     lines = File.ReadLines(fileName).ToList();
-} 
-catch (FileNotFoundException) 
+}
+catch (FileNotFoundException ex)
 {
-    DisplayError("File not found.");
+    DisplayError($"File {Path.GetFileName(ex.FileName)} not found.");
     Environment.Exit(2);
 }
 catch (IOException)
@@ -28,27 +31,67 @@ catch (Exception)
     Environment.Exit(4);
 }
 
+// Split the lines
 string[] headerCols = lines[0].Split("\t");
 
-foreach (string col in headerCols)
+if (!(headerCols.Contains(args[1]) || headerCols.Contains(args[2])))
 {
-    if (args[1] == col)
-    {
-        groupByCol = col;
-    }
-    else if (args[2] == col)
-    {
-        numericCol = col;
-    }
+    DisplayError("Group by column or numeric column does not exist.");
+    Environment.Exit(5);
 }
 
-if (groupByCol == null || numericCol == null)
-{
+int groupByColIndex = Array.IndexOf(headerCols, args[1]);
+int numericColIndex = Array.IndexOf(headerCols, args[2]);
 
-}
 lines.Remove(lines[0]); // Remove header line for easier handling
 
+List<Data> dataList = ReadData(lines, groupByColIndex, numericColIndex).ToList();
+var result = dataList
+    .GroupBy(n => n.GroupByCol)
+    .Select(group =>
+    {
+        return new
+        {
+            GroupByKey = group.Key,
+            NumericColSum = group.Sum(n => int.Parse(n.NumericCol))
+        };
+    })
+    .OrderByDescending(n => n.NumericColSum);
 
+int maxCount = (args.Length == 4) ? int.Parse(args[3]) : result.Count();
+int maxLength = result.Take(maxCount).Max(n => n.GroupByKey.Length);
+int baseAmount = result.First().NumericColSum;
+
+for (int i = 0; i < maxCount; i++)
+{
+    Console.Write(result.ElementAt(i).GroupByKey.PadLeft(maxLength) + " | ");
+    Console.BackgroundColor = ConsoleColor.Red;
+    for (int j = 1; j <= result.ElementAt(i).NumericColSum * 100 / baseAmount; j++)
+    {
+        Console.Write(" ");
+    }
+    Console.ResetColor();
+    Console.WriteLine();
+}
+
+/// <summary>
+/// Reads data from two columns.
+/// </summary>
+static IEnumerable<Data> ReadData(List<string> source, int index1, int index2)
+{
+    foreach (string line in source)
+    {
+        yield return new Data()
+        {
+            GroupByCol = line.Split("\t")[index1],
+            NumericCol = line.Split("\t")[index2]
+        };
+    }
+}
+
+/// <summary>
+/// Displays an error in the console.
+/// </summary>
 static void DisplayError(string message)
 {
     Console.ForegroundColor = ConsoleColor.Red;
