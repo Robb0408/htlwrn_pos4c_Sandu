1a5bd1ab35cfdfa3251a41ab2a8e13536ceaf267
sandu.emanuel <sandu.emanuel@student.htlwrn.ac.at>
2023-10-30 12:12:32 +0100

    class for json objects

---
+++ E6 - Website Analytics/LogAnalysis.Logic/Photo.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LogAnalysis.Logic
+{
+	public class Photo
+	{
+		[JsonPropertyName("pic")]
+		public string Pic { get; set; }
+
+		[JsonPropertyName("takenBy")]
+		public string TakenBy { get; set; }
+
+		[JsonPropertyName("uploadYear")]
+		public int UploadYear { get; set; }
+	}
+}
+
