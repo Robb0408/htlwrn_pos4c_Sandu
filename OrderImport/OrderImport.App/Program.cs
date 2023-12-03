4c0f8b4098675e41cf9eebc77bfeb82a87418d4e
sandu.emanuel <sandu.emanuel@student.htlwrn.ac.at>
2023-12-03 20:15:12 +0100

    Add Day 1: Part 1 & 2

---
+++ OrderImport/OrderImport.App/Program.cs
@@ -0,0 +1,19 @@
+﻿var command = args[0];
+switch (command)
+{
+    case "import":
+        Console.WriteLine("a");
+        break;
+    case "clean":
+        Console.WriteLine("b");
+        break;
+    case "check":
+        Console.WriteLine("c");
+        break;
+    case "full":
+        Console.WriteLine("d");
+        break;
+    default:
+        Console.WriteLine("e");
+        break;
+}
