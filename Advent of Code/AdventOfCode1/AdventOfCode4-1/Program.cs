227935e55a883ab7843142911f5798947647b8f2
sandu.emanuel <sandu.emanuel@student.htlwrn.ac.at>
2023-12-07 18:58:50 +0100

    Add Day 4 Part 1

---
+++ Advent of Code/AdventOfCode1/AdventOfCode4-1/Program.cs
@@ -0,0 +1,37 @@
+﻿var lines = await File.ReadAllLinesAsync("cards.txt");
+var cards = lines.Select(line => line.Split(": ")[1]).ToList();
+var totalPoints = new List<int>();
+
+cards.ForEach(card =>
+{
+    var sum = 0;
+    var isDouble = false;
+    var winning = card
+        .Split(" | ")[0].Split(" ")
+        .Where(num => num.Length > 0)
+        .Select(int.Parse);
+    var numbers = card
+        .Split(" | ")[1].Split(" ")
+        .Where(num => num.Length > 0)
+        .Select(int.Parse);
+    foreach (var num in numbers)
+    {
+        foreach (var s in winning)
+        {
+            if (num != s) continue;
+            if (isDouble)
+            {
+                sum *= 2;
+            }
+            else
+            {
+                sum++;
+                isDouble = true;
+            }
+        }
+    }
+
+    totalPoints.Add(sum);
+});
+
+Console.WriteLine(totalPoints.Sum());
