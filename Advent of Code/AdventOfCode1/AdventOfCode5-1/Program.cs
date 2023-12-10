f0149b41332dc3a6ec00d2c00a7676da21492d06
sandu.emanuel <sandu.emanuel@student.htlwrn.ac.at>
2023-12-10 23:40:15 +0100

    Add Day 5 Part 1 & 2

---
+++ Advent of Code/AdventOfCode1/AdventOfCode5-1/Program.cs
@@ -0,0 +1,131 @@
+﻿var content = await File.ReadAllLinesAsync("seeds.txt");
+
+var seeds = content[0].Split(": ")[1].Split(" ")
+    .Select(n => new Seed { Number = long.Parse(n) });
+
+content = content.Skip(2).ToArray();
+var completeSeeds = new List<Seed>();
+
+foreach (var seed in seeds)
+{
+    var isSoil = false;
+    var isFertilizer = false;
+    var isWater = false;
+    var isLight = false;
+    var isTemperature = false;
+    var isHumidity = false;
+    var mapping = new List<long[]>();
+
+    foreach (var line in content)
+    {
+        var chars = line.ToCharArray();
+        if (chars.Length > 0 && !char.IsNumber(chars[0])) continue;
+        if (!isSoil && line == string.Empty)
+        {
+            // If line empty, then go to next mapping
+            seed.Soil = Map(seed.Number, mapping);
+            isSoil = true;
+            mapping.Clear();
+        }
+        else if (!isFertilizer && line == string.Empty)
+        {
+            // If line empty, then go to next mapping
+            seed.Fertilizer = Map(seed.Soil, mapping);
+            isFertilizer = true;
+            mapping.Clear();
+        }
+        else if (!isWater && line == string.Empty)
+        {
+            // If line empty, then go to next mapping
+            seed.Water = Map(seed.Fertilizer, mapping);
+            isWater = true;
+            mapping.Clear();
+        }
+        else if (!isLight && line == string.Empty)
+        {
+            // If line empty, then go to next mapping
+            seed.Light = Map(seed.Water, mapping);
+            isLight = true;
+            mapping.Clear();
+        }
+        else if (!isTemperature && line == string.Empty)
+        {
+            // If line empty, then go to next mapping
+            seed.Temperature = Map(seed.Light, mapping);
+            isTemperature = true;
+            mapping.Clear();
+        }
+        else if (!isHumidity && line == string.Empty)
+        {
+            // If line empty, then go to next mapping
+            seed.Humidity = Map(seed.Temperature, mapping);
+            isHumidity = true;
+            mapping.Clear();
+        }
+        else
+        {
+            var split = line.Split(" ");
+            mapping.Add(new[]
+            {
+                long.Parse(split[0]),
+                long.Parse(split[1]), long.Parse(split[2])
+            });
+        }
+    }
+
+    seed.Location = Map(seed.Humidity, mapping);
+    mapping.Clear();
+    completeSeeds.Add(seed);
+}
+
+foreach (var seed in completeSeeds)
+{
+    Console.WriteLine($"Seed {seed.Number} is at location {seed.Location}" +
+                      $"\n\tSoil: {seed.Soil}" +
+                      $"\n\tFertilizer: {seed.Fertilizer}" +
+                      $"\n\tWater: {seed.Water}" +
+                      $"\n\tLight: {seed.Light}" +
+                      $"\n\tTemperature: {seed.Temperature}" +
+                      $"\n\tHumidity: {seed.Humidity}" +
+                      $"\n\tLocation: {seed.Location}");
+}
+
+static long Map(long numberToMap, IEnumerable<long[]> mapping)
+{
+    var result = numberToMap;
+    var isDone = false;
+    foreach (var numbers in mapping)
+    {
+        var destination = numbers[0];
+        var source = numbers[1];
+        var range = numbers[2];
+
+        for (var i = 0; i < range; i++)
+        {
+            if (result == source + i)
+            {
+                result = destination + i;
+                isDone = true;
+                break;
+            }
+        }
+        if (isDone) { break; }
+    }
+
+    Console.WriteLine($"Mapped {numberToMap} to {result}");
+    return result;
+}
+
+Console.WriteLine(completeSeeds.Min(seed => seed.Location));
+
+public class Seed
+{
+    public long Number { get; init; }
+    public long Soil { get; set; }
+    public long Fertilizer { get; set; }
+    public long Water { get; set; }
+    public long Light { get; set; }
+    public long Temperature { get; set; }
+    public long Humidity { get; set; }
+    public long Location { get; set; }
+}
