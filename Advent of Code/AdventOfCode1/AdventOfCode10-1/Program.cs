1504643195b7daf95322c10052e3aa0e77b8c712
sandu.emanuel <sandu.emanuel@student.htlwrn.ac.at>
2024-01-02 17:32:10 +0100

    Add Day 3-2 and 10-1 (both not finished)

---
+++ Advent of Code/AdventOfCode1/AdventOfCode10-1/Program.cs
@@ -0,0 +1,47 @@
+﻿var content = await File.ReadAllLinesAsync("pipes.txt");
+
+var lines = content.Select(lines => lines.ToCharArray().ToList()).ToList();
+
+var startRow = 0;
+var startCol = 0;
+var isFound = false;
+for (var i = 0; i < lines.Count; i++)
+{
+    for (var j = 0; j < lines[i].Count; j++)
+    {
+        if (lines[i][j] == 'S')
+        {
+            startRow = i;
+            startCol = j;
+            isFound = true;
+            break;
+        }
+
+        startCol++;
+    }
+    if (isFound)
+    {
+        break;
+    }
+
+    startRow++;
+}
+
+Console.WriteLine("Start: " + startRow + ", " + startCol);
+
+var endRow = -1;
+var endCol = -1;
+
+// | vertical
+// - horizontal
+// L top <-> right
+// J top <-> left
+// 7 bottom <-> left
+// F bottom <-> right
+// . no pipe
+// S start
+
+while (endCol == -1 && endRow == -1)
+{
+    
+}
