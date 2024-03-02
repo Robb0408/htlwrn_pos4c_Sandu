fb1988264d4072454575ce3330c7b219ce819e6e
Robb0408 <emanuelrobertosandu@gmail.com>
2024-03-02 16:17:14 +0100

    backup

--- CollatzConjecture/CollatzConjecture.Api/Controllers/NumberController.cs
+++ CollatzConjecture/CollatzConjecture.Api/Controllers/NumberController.cs
@@ -1,6 +1,32 @@
+using CollatzConjecture.Logic;
+using Microsoft.AspNetCore.Mvc;
+
 namespace CollatzConjecture.Api.Controllers;
 
-public class NumberController
+[ApiController]
+[Route("[controller]")]
+public class NumberController : ControllerBase
 {
+    private ICollatzConjectureService service;
     
+    public NumberController(ICollatzConjectureService service)
+    {
+        this.service = service;
+    }
+    
+    [HttpGet("/{number}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<int>))]
+    public IActionResult GetSequence(int number)
+    {
+        var sequence = service.GetSequence(number);
+        return Ok(sequence);
+    }
+    
+    [HttpGet("/valid/{number}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+    public IActionResult IsSequenceValid(int number)
+    {
+        var result = service.IsSequenceValid(number);
+        return Ok(result);
+    }
 }
--- CollatzConjecture/CollatzConjecture.Api/Controllers/StringController.cs
+++ CollatzConjecture/CollatzConjecture.Api/Controllers/StringController.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace CollatzConjecture.Api.Controllers;
 
+[ApiController]
 public class StringController
 {
     
